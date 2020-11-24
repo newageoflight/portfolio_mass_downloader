88f0317e54cb33d29c29ec9a6f82a5257232b76c
newageoflight <camint3rnal@live.com>
2020-11-25 10:39:13 +1100

    Adds zID clarification

--- setup.py
+++ setup.py
@@ -1,4 +1,21 @@
-from distutils.core import setup
-import py2exe
+from cx_Freeze import setup, Executable
 
-setup(console=["main.py"])
+base = None
+
+executables = [Executable("main.py", base=base)]
+
+packages=["idna", "crawlers", "os", "json", "requests_html", "lxml", "tqdm", "urllib.parse", "traceback",
+"multiprocessing", "pathlib", "csv", "requests"]
+options = {
+    "build_exe": {
+        "packages": packages
+    }
+}
+
+setup(
+    name="Portfolio Mass Downloader",
+    options=options,
+    version="0.1.1.0",
+    description="Downloads your entire portfolio from eMed",
+    executables=executables
+)
